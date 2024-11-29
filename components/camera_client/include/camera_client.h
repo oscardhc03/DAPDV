ac02e52112c4b8f35c8aaf811757907ff58dfa0a
FrozenBurrito <f3rm3ndouza@outlook.com>
2024-11-29 09:15:02 -0600

    Peticion HTTP GET para obtener resultados de inferencia

--- components/camera_client/include/camera_client.h
+++ components/camera_client/include/camera_client.h
@@ -8,9 +8,24 @@
 #include <esp_netif.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
+#include <esp_http_client.h>
+#include <esp_tls.h>
+
+#include <cJSON.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/event_groups.h>
+
+
+/**
+ * Resultado de la inferencia en la imagen enviada por la cámara al servidor.
+ */
+typedef struct _cam_detect_objects_t {
+    float persona;
+    float escaleras;
+    float entorno;
+    float puerta;
+} cam_detect_objects_t;
 
 /**
  * @brief Inicializa el cliente HTTP que obtiene los resultados de inferencia de la cámara.
@@ -19,10 +34,25 @@
  * 
  * La SSID y contraseña del AP se definen en menuconfig.
  * 
- * @return ESP_OK si inicializó correctamente el cliente HTTP.
- * @return ESP_ERR_INVALID_ARG si uno de los valores configurados en kconfig no es válido.
- * @return ESP_FAIL si ocurrió algún otro problema.
+ * @return - ESP_OK si inicializó correctamente el cliente HTTP.
+ * @return - ESP_ERR_INVALID_ARG si uno de los valores configurados en kconfig no es válido.
+ * @return - ESP_FAIL si ocurrió algún otro problema.
  */
 esp_err_t camera_client_init(void);
+
+/**
+ * @brief Obtiene los resultados de detección de objetos desde un servidor intermedio.
+ * 
+ * Para obtener los resultados, usa un cliente HTTP para hacer una petición GET
+ * al endpoint "predict" del servicio. Luego interpreta la respuesta con JSON.
+ * 
+ * @param results referencia a la estructura donde almacenará los resultados.
+ * 
+ * @return - ESP_OK obtuvo correctamente los resultados de la cámara.
+ * @return - ESP_ERR_INVALID_ARG si results es NULL.
+ * @return - ESP_ERR_INVALID_STATE si no estaba inicializado el cliente con camera_client_init().
+ * @return - ESP_FAIL si ocurrió algún otro error.
+ */
+esp_err_t camera_client_fetch_results(cam_detect_objects_t * const results);
 
 #endif /* CAMERA_CLIENT_H_ */
--- main/main.c
+++ main/main.c
@@ -581,20 +581,20 @@ void distance_sensor_task(void * pvParameters)
 
 void object_detection_task(void * pvParameters)
 {
-    static const uint32_t TEST_EVENT_PERIOD_SECONDS = 10UL;
+    static const TickType_t SEND_REQUEST_PERIOD_TICKS = pdMS_TO_TICKS(15000);
     static const TickType_t SEND_OBJECT_DETECT_EVENT_TIMEOUT_TICKS = pdMS_TO_TICKS(100);
 
+    static const float OBJECT_DETECT_THRESHOLD = 0.75f;
+
     esp_err_t status;
-    uint32_t seconds_before_next_test_event;
-    BaseType_t did_detect_object;
+    cam_detect_objects_t detected_objects;
 
     status = ESP_OK;
-    seconds_before_next_test_event = TEST_EVENT_PERIOD_SECONDS;
-    did_detect_object = pdFALSE;
 
     if (ESP_OK == status)
     {
         status = camera_client_init();
+        ESP_LOGI(TAG, "Camera client init complete (%s)", esp_err_to_name(status));
     }
 
     if (ESP_OK != status)
@@ -605,32 +605,41 @@ void object_detection_task(void * pvParameters)
 
     while (1)
     {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(SEND_REQUEST_PERIOD_TICKS);
 
-        seconds_before_next_test_event--;
+        ESP_LOGI(TAG, "Fetching camera results...");
+        status = camera_client_fetch_results(&detected_objects);
 
-        if (0UL == seconds_before_next_test_event)
+        if (ESP_OK == status)
         {
-            seconds_before_next_test_event = TEST_EVENT_PERIOD_SECONDS;
-            did_detect_object = !did_detect_object;
+            ESP_LOGI(TAG, "Persona = %.6f", detected_objects.persona);
+            ESP_LOGI(TAG, "Escaleras = %.6f", detected_objects.escaleras);
+            ESP_LOGI(TAG, "Entorno = %.6f", detected_objects.entorno);
+            ESP_LOGI(TAG, "Puerta = %.6f", detected_objects.puerta);
 
-            if (pdTRUE == did_detect_object)
+            if (OBJECT_DETECT_THRESHOLD < detected_objects.persona || OBJECT_DETECT_THRESHOLD < detected_objects.escaleras || OBJECT_DETECT_THRESHOLD < detected_objects.puerta)
             {
-                ESP_LOGI(TAG, "Test object detected");
-
+                // Si es cualquiera de los objetos anteriores, enviar evento para detener.
                 status = hmi_feedback_event_send(FEEDBACK_EVENT_STOP, FEEDBACK_SOURCE_OBJECT_DETECT, FEEDBACK_PRIORITY_NORMAL, SEND_OBJECT_DETECT_EVENT_TIMEOUT_TICKS);
+                
+                if (ESP_OK != status)
+                {
+                    ESP_LOGW(TAG, "Object detect feedback event send fail (%s)", esp_err_to_name(status));
+                }
             }
-            else
+            else if (OBJECT_DETECT_THRESHOLD < detected_objects.entorno)
             {
-                ESP_LOGI(TAG, "Test object no longer detected");
+                status = hmi_feedback_event_send(FEEDBACK_EVENT_STRAIGHT, FEEDBACK_SOURCE_OBJECT_DETECT, FEEDBACK_PRIORITY_NORMAL, SEND_OBJECT_DETECT_EVENT_TIMEOUT_TICKS);
 
-                status = hmi_feedback_event_send(FEEDBACK_EVENT_STRAIGHT, FEEDBACK_SOURCE_OBJECT_DETECT, FEEDBACK_PRIORITY_LOW, SEND_OBJECT_DETECT_EVENT_TIMEOUT_TICKS);
+                if (ESP_OK != status)
+                {
+                    ESP_LOGW(TAG, "Object detect feedback event send fail (%s)", esp_err_to_name(status));
+                }
             }
-
-            if (ESP_OK != status)
-            {
-                ESP_LOGE(TAG, "Object detect feedback event send fail (%s)", esp_err_to_name(status));
-            }
+        }
+        else
+        {
+            ESP_LOGW(TAG, "HTTP request fail (%s)", esp_err_to_name(status));
         }
     }
 
